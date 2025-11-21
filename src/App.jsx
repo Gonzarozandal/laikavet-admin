d32a2ba21c6b11287ff4f39c199e36aec5313b57
NachoGaynor2003 <nachoteam14@gmail.com>
2025-11-20 21:02:15 -0300

    Se agrega la pagina Mascotas en blanco

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Homepages from './pages/Homepages.jsx';
 import HistorialClinicaPage from './pages/HistorialClinicaPage.jsx';
 import FormUsersPage from './Components/FormUsers.jsx';
-
+import MascotasPage from './pages/MascotasPage.jsx';
 function App() {
   return (
-    <Routes>    
-      <Route path='/' element={<Homepages/>} />
+    <Routes>
+      <Route path="/" element={<Homepages />} />
       <Route path="/HistorialClinicaPage" element={<HistorialClinicaPage />} />
+      <Route path="/MascotasPage" element={<MascotasPage />} />
       <Route path="/FormUsersPage" element={<FormUsersPage />} />
     </Routes>
   );
---
+++ src/pages/MascotasPage.jsx
@@ -0,0 +1,9 @@
+
+function MascotasPage() {
+  return (
+    <div>
+      <p>aca van las mascotas</p>
+    </div>
+  );
+}
+export default MascotasPage;
